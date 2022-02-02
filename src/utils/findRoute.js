5882d541b08a0f20bda1cc0b762012a336872b8c
Rafael Ramos <rafael.cleydson@gmail.com>
2022-02-02 01:41:28 -0300

    Adicionando logica para encontrar menor caminho entre as conexões
    Co-authored-by: Eduardo Afonso <oeduardoafonso@gmail.com>

--- src/utils/findRoute.js
+++ src/utils/findRoute.js
@@ -1,9 +1,12 @@
 const networkGraph = require("../data/graph.json");
 
+let nodeParents = new Array(26);
+
 const BFS = (startLAN, endLAN) => {
   const queue = [];
   const visited = [];
   const startNODE = networkGraph[startLAN].router;
+  nodeParents[startNODE] = startLAN;
 
   visited.push(startNODE);
 
@@ -12,9 +15,9 @@ const BFS = (startLAN, endLAN) => {
   while (queue.length > 0) {
     let visiting = queue.shift();
 
-    console.log(`Visitei ${networkGraph[visiting.id].name}`)
     if (visiting.connectedLAN.find((el) => endLAN == el) != undefined) {
-      console.log(`Cheguei em ${networkGraph[endLAN].name}`)
+      nodeParents[endLAN] = visiting.id;
+      showPath(startLAN, endLAN);
       break;
     }
 
@@ -22,10 +25,24 @@ const BFS = (startLAN, endLAN) => {
       if (visited.find((el) => el == indexNeighbor) == undefined) {
         visited.push(indexNeighbor);
         queue.push(networkGraph[indexNeighbor]);
-
+        nodeParents[indexNeighbor] = visiting.id;
+        // console.log(`nodeParents: ${nodeParents}`)
+        // console.log(`FILHO: ${networkGraph[indexNeighbor].name} [${indexNeighbor}] PAI: ${networkGraph[visiting.id].name} [${visiting.id}]`);
       }
     });
   }
+};
+
+const showPath = (startLAN, endLAN) => {
+  const path = [];
+  let nodeParent = networkGraph[endLAN].id;
+  while (nodeParent != startLAN) {
+    path.unshift(networkGraph[nodeParent].name);
+    nodeParent = nodeParents[nodeParent];
+  }
+  path.unshift(networkGraph[nodeParent].name);
+
+  console.log(path);
 };
 
 BFS(25, 14);
